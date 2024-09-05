2b40900c5c255672fad8ed94416ae2f739dae799
omondi-mutuma <omondimutuma@gmail.com>
2024-09-05 11:48:42 +0300

    Responsive supplements

--- app/ui/GridBg.tsx
+++ app/ui/GridBg.tsx
@@ -7,8 +7,8 @@ import CardDemo from "@/components/blocks/cards-demo-1";
 const GridBg = () => {
   return (
     <section className="content-container relative min-h-screen bg-default-bg-rgb flex">
-      <div className="w-full flex flex-col items-start justify-center lg:max-w-lg">
-        <h1 className="text-5xl lg:text-7xl font-semibold text-gray-500">
+      <div className="w-full flex flex-col items-start justify-center z-10 lg:max-w-lg">
+        <h1 className="text-6xl lg:text-7xl font-semibold text-gray-500">
           The Future <br></br>
           <span className="text-slate-950">of Sustainable</span> Energy
         </h1>
@@ -23,7 +23,7 @@ const GridBg = () => {
           icon={BsArrowRightCircleFill}
         />
       </div>
-      <div className="hidden absolute right-80 bottom-0 top-1/2 -translate-y-1/2 lg:flex flex-col justify-center">
+      <div className="hidden absolute right-80 bottom-0 top-1/2 -translate-y-1/2 lg:flex flex-col justify-center z-10">
         <video className="max-w-2xl max-h-[600px]" muted loop autoPlay>
           <source
             src="/8741596-hd_720_1280_30fps.mp4"
@@ -31,7 +31,7 @@ const GridBg = () => {
           ></source>
         </video>
       </div>
-      <div className="hidden absolute right-0 bottom-0 top-1/2 -translate-y-1/2 lg:flex flex-col justify-center">
+      <div className="hidden absolute right-0 bottom-0 top-1/2 -translate-y-1/2 lg:flex flex-col justify-center z-10">
         <video className="max-w-2xl max-h-[600px]" muted loop autoPlay>
           <source
             src="/8741596-hd_720_1280_30fps.mp4"
@@ -39,6 +39,9 @@ const GridBg = () => {
           ></source>
         </video>
       </div>
+      <div className="absolute -top-[50%] -left-[50%] h-[500px] w-[500px] border border-slate-300 rounded-full lg:-top-[40%] lg:-left-[20%]"></div>
+      <div className="absolute -top-[50%] -left-[50%] h-[500px] w-[500px] border border-slate-300 rounded-full lg:-top-[60%] lg:-left-[5%]"></div>
+      <div className="absolute -top-[50%] -left-[50%] h-[1000px] w-[500px] border border-slate-300 rounded-full rotate-45 lg:-top-[60%] lg:left-[10%]"></div>
     </section>
   );
 };
